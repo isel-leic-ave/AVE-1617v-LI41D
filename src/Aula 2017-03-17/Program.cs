bb9ad547c09f3e818070842f027e4a92e143c718
Luís Falcão <lfalcao28@gmail.com>
2017-03-17 15:21:47 +0000

    Aula 2017-03-17 - API de reflexão em .NET

---
+++ src/Aula 2017-03-17/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Reflection;
+
+public class Program : Object {
+	// Membros
+	
+	// Fields
+	private int instanceField; // Instance Field
+	private static int classField; // Class Field
+	
+	
+	// Methods
+	public void instanceMethod() { } // Instance Method
+	public static void classMethod() { } // Class Method
+	
+	
+	// Other Types
+	class InnerClass {
+		
+	}
+	
+	public static void Main() {
+		Console.Write("Using reflection API");
+		
+		Type t = typeof(Program);
+		
+		MemberInfo[] mi = t.GetMembers();
+		for(int i = 0; i < mi.Length; ++ i) {
+				Console.WriteLine(mi[i].Name);
+		}
+	}
+
+}
