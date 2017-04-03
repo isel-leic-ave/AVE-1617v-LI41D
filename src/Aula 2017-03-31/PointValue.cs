13928fb7508d324a25b8df8d061377025e131172
lfalcao28 <lfalcao28@gmail.com>
2017-04-03 09:57:01 +0100

    Aula 2017-03-31 - Reference Types vs Value Types

---
+++ src/Aula 2017-03-31/PointValue.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Aula_2017_03_31
+{
+    public struct PointValue
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public PointValue(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+    }
+}
---
+++ src/Aula 2017-03-31/TestingTypes.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Aula_2017_03_31
+{
+    public class TestingTypes
+    {
+        public static void Main(string[] args)
+        {
+            int x = 5;
+
+            int y = x;
+
+            x = 6;
+
+
+            PointRef pr = new PointRef(2,3);
+            PointRef pr1 = new PointRef(2, 3);
+
+            PointRef pr2 = pr.Add(pr1);
+            PointRef pr3 = pr2.Add(pr1);
+
+            PointRef pr4 = pr3;
+
+            Console.WriteLine(Object.ReferenceEquals(pr3, pr4));
+            
+
+
+            ChangeAndShow(pr);
+            Console.WriteLine("Ouside ChangeAndShow: " + pr.X);
+
+            PointValue pv = new PointValue(4,5);
+            ChangeAndShow(pv);
+            Console.WriteLine("Ouside ChangeAndShow: " + pv.X);
+
+
+            PointValue pv1 = pv;
+            Console.WriteLine(Object.ReferenceEquals(pv1, pv));
+
+            Console.WriteLine(Object.ReferenceEquals(pv, pv));
+
+
+
+
+
+        }
+
+        static void ChangeAndShow(PointRef p)
+        {
+            p.X++;
+            Console.WriteLine("Inside ChangeAndShow: " + p.X);
+
+        }
+
+        static void ChangeAndShow(PointValue pv)
+        {
+            pv.X++;
+            Console.WriteLine("Inside ChangeAndShow: " + pv.X);
+        }
+    }
+}
