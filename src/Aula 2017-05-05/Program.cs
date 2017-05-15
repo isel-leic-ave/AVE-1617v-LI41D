f0411ab83ae9592171526bd771b790e02c71487d
lfalcao28 <lfalcao28@gmail.com>
2017-05-15 11:23:58 +0100

    Aula 2017-05-05 - Chamada a métodos virtuais.

---
+++ src/Aula 2017-05-05/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_2017_05_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Base f = new Base();
+            M1(f);
+
+            Base f1 = new Derived1();
+            M1(f1);
+        }
+
+        private static void M1(Base b)
+        {
+            b.Method1();
+            b.Method2();
+        }
+    }
+
+    internal class Derived1 : Base
+    {
+    }
+
+    internal class Base
+    {
+        public void Method1()
+        {
+            Console.WriteLine("Method 1 in Base");
+        }
+
+        public void Method2()
+        {
+            Console.WriteLine("Method 2 in Base");
+        }
+    }
+}
