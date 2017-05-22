a73cc5f572d40b3460db782743cd86c0943f0a74
lfalcao28 <lfalcao28@gmail.com>
2017-05-22 11:10:11 +0100

    Aula 2017-05-22-Classes e delegates genericos

---
+++ src/Aula2017-05-22/Program.cs
@@ -0,0 +1,39 @@
+﻿
+
+
+using System;
+using System.Security.Cryptography;
+using Aula_2017_05_22;
+
+namespace Aula2017_05_22
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Stack<int> sInt = new Stack<int>(10);
+            sInt.Push(1);sInt.Push(3);sInt.Push(2);sInt.Push(5);
+
+            
+
+            int maxInt = sInt.Max();
+            Console.WriteLine(maxInt);
+
+            Stack<string> sStr = new Stack<string>(10);
+
+
+            sStr.Push("aa"); sStr.Push("c"); sStr.Push("b"); sStr.Push("d");
+
+            string maxStr = sStr.Max();
+            Console.WriteLine(maxStr);
+
+
+            string maxStr1 = sStr.Max((s1,s2) => s1.Length - s2.Length);
+            Console.WriteLine(maxStr1);
+
+
+        }
+    }
+}
---
+++ src/Aula2017-05-22/Stack.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Aula_2017_05_22
+{
+    public class Stack<T> where T: IComparable<T> 
+    {
+        private T []values;
+        private int idx;
+
+        public Stack(int limit)
+        {
+            Type t = typeof(T);
+            Console.WriteLine(t.Name);
+
+            values = new T[limit];
+            idx = 0;
+        }
+
+        public void Push(T i)
+        {
+            if (idx == values.Length)
+            {
+                throw new InvalidOperationException("Push on an full stack");
+            }
+            values[idx++] = i;
+        }
+
+        public T Pop()
+        {
+            if (idx == 0)
+            {
+                throw new InvalidOperationException("Pop on an empty stack");
+            }
+            return values[--idx];
+        }
+
+        public T Max()
+        {
+            T max = default(T);
+            for (int i = 0; i < idx; i++)
+            {
+                if (values[i].CompareTo(max) > 0)
+                    max = values[i];
+            }
+
+            return max;
+        }
+
+
+        //public delegate int Comparable(T t1, T t2);
+
+        public T Max(Comparison<T> cmp)
+        {
+            T max = idx == 0 ? default(T) : values[0];
+            for (int i = 1; i < idx; i++)
+            {
+                if (cmp(values[i], max) > 0)
+                    max = values[i];
+            }
+
+            return max;
+        }
+    }
+}
