f5b6580e1faa2b082e09304035c5827d48599658
lfalcao28 <lfalcao28@gmail.com>
2017-06-09 16:44:31 +0100

    Aula 2017-06-09-Implementação explícita de interfaces. Tipos Nullable.

---
+++ src/Aula 2017-06-09/IMapper.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+
+namespace Aula_2017_06_09
+{
+    public interface IMapper<TSrc, TDest> : IMapper
+    {
+        TDest Map(TSrc src);
+        TDest[] Map(TSrc[] src);
+    }
+
+    public interface IMapper
+    {
+        Object Map(Object src);
+        Object[] Map(Object[] src);
+    }
+
+
+    public class Mapper : IMapper
+    {
+        public Object Map(Object src)
+        {
+            return null;
+        }
+
+        public Object[] Map(Object[] src)
+        {
+            return null;
+        }
+    }
+
+
+    public class Mapper<TSrc, TDest> : IMapper<TSrc, TDest>
+    {
+        public TDest Map(TSrc src)
+        {
+            return (TDest) ((IMapper)this).Map(src);
+        }
+
+        public TDest[] Map(TSrc[] src)
+        {
+            return null;
+        }
+
+
+        Object IMapper.Map(Object src)
+        {
+            return this.Map((TSrc)src);
+        }
+
+        Object[] IMapper.Map(Object[] src)
+        {
+            TSrc[] array = src.OfType<TSrc>().ToArray();
+            return Map(array).OfType<Object>().ToArray();
+        }
+    }
+
+
+
+    class AutoMapper
+    {
+        Mapper Build()
+        {
+            return null;
+        }
+
+        Mapper<T,D> Build<T, D>()
+        {
+            return new Mapper<T, D>();
+        }
+    }
+}
---
+++ src/Aula 2017-06-09/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_2017_06_09
+{
+    interface IA
+    {
+        void m1();
+        void m2();
+    }
+
+
+    interface IB
+    {
+        int m1();
+        void m3();
+    }
+
+    class C1 : IA, IB
+    {
+        public void m1()
+        {
+
+        }
+
+        int IB.m1()
+        {
+            return 0;
+        }
+
+
+        void IA.m2()
+        {
+            
+        }
+
+        public void m3()
+        {
+
+        }
+
+    }
+
+    class Program
+    {
+
+        static IA Foo()
+        {
+            return null;
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            // Interface explicit implementation
+            //C1 c = new C1();
+            //IA ia = c;
+            //ia.m1();
+
+            //Foo().m1();
+
+            //((IA)c).m1();
+
+
+
+            // NUllable types
+
+            Nullable<int> i = null;
+
+            Console.WriteLine(i.HasValue);
+
+            Console.WriteLine(i.Value);
+
+
+
+
+        }
+    }
+}
